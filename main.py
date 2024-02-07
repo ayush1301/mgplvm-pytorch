3b457f95cf4d0371587be3fdaf044c128a9c1e22
ayush_1301 <ayush.ag1301@gmail.com>
2024-02-07 20:38:01 +0000

    new code for recognition model + some minor changes

--- main.py
+++ main.py
@@ -89,27 +89,35 @@ class GenerativeModel(Module, metaclass=abc.ABCMeta):
         optimizer = optimizer(self.parameters(), lr=lrate)
         scheduler = StepLR(optimizer, step_size=train_params['step_size'], gamma=train_params['gamma']) # TODO: understand these parameters
 
-        LLs = []
+        self.LLs = []
         for i in range(max_steps):
             prev_z = None
             for z, y in data: # loop over batches
                 loss = -self.joint_LL(n_mc, z, y, prev_z).mean() # TODO: should I use mean??
                 loss.backward()
-                LLs.append(-loss.item())
+                self.LLs.append(-loss.item())
                 optimizer.step()
                 optimizer.zero_grad()
                 prev_z = z[..., -1] # (ntrials, b)
             scheduler.step()
             if i % train_params['print_every'] == 0:
-                print('step', i, 'LL', LLs[-1])
+                print('step', i, 'LL', self.LLs[-1])
     
     def freeze_params(self):
         for param in self.parameters():
             param.requires_grad = False
 
+    def plot_LL(self):
+        plt.plot(self.LLs)
+        plt.xlabel('Step')
+        plt.ylabel('LL')
+        plt.show()
+
 
 class LDS(GenerativeModel):
-    def __init__(self, z: Tensor, Y: Tensor, lik, x_dim=None, link_fn=torch.exp) -> None:
+    def __init__(self, z: Tensor, Y: Tensor, lik, x_dim=None, link_fn=torch.exp,
+                 A=None, C=None, W=None, B=None, mu0=None, Sigma0_half=None, sigma_x=None,
+                 trained_z=False) -> None:
         super().__init__(z, Y, lik)
 
         self.link_fn = link_fn
@@ -120,21 +128,28 @@ class LDS(GenerativeModel):
             self.x_dim = x_dim
 
         # Generative parameters
-        A = torch.rand(1, self.b, self.b).to(device)
-        C = torch.rand(1, self.N, self.x_dim).to(device)
-        W = torch.rand(1, self.x_dim, self.b).to(device)
-        B = torch.rand(1, self.b, self.b).to(device)
-        mu0 = torch.rand(1, self.b).to(device)
-        Sigma0_half = 0.1 * torch.eye(self.b)[None, ...].to(device)
-        sigma_x = torch.abs(torch.randn(1).to(device))
+        if A is None:
+            A = torch.rand(1, self.b, self.b).to(device)
+        if C is None:
+            C = torch.rand(1, self.N, self.x_dim).to(device)
+        if W is None:
+            W = torch.rand(1, self.x_dim, self.b).to(device)
+        if B is None:
+            B = torch.rand(1, self.b, self.b).to(device)
+        if mu0 is None:
+            mu0 = torch.rand(1, self.b).to(device)
+        if Sigma0_half is None:
+            Sigma0_half = 0.1 * torch.eye(self.b)[None, ...].to(device)
+        if sigma_x is None:
+            sigma_x = torch.abs(torch.randn(1).to(device))
 
-        self.A = torch.nn.Parameter(A)
+        self.A = torch.nn.Parameter(A, requires_grad= not trained_z)
         self.C = torch.nn.Parameter(C)
         self.W = torch.nn.Parameter(W)
-        self.B = torch.nn.Parameter(B)
+        self.B = torch.nn.Parameter(B, requires_grad= not trained_z)
         self.sigma_x = torch.nn.Parameter(sigma_x)
-        self.mu0 = torch.nn.Parameter(mu0)
-        self.Sigma0_half = torch.nn.Parameter(Sigma0_half)
+        self.mu0 = torch.nn.Parameter(mu0, requires_grad= not trained_z)
+        self.Sigma0_half = torch.nn.Parameter(Sigma0_half, requires_grad= not trained_z)
 
         # print(self.A.shape, self.B.shape, self.C.shape, self.sigma_x.shape, self.mu0.shape, self.Sigma0.shape)
     
@@ -300,7 +315,10 @@ class RecognitionModel(Module):
         
     def train_recognition_model(self, train_params_recognition):
         # dataset = Dataset(self.gen_model.Y)
-        dataloader = DataLoader(self.gen_model.Y, batch_size=train_params_recognition['batch_size'])
+
+        # So that the last dimension is the batch dimension
+        transposed_Y = self.gen_model.Y.transpose(0, -1) # (T, N, ntrials)
+        dataloader = DataLoader(transposed_Y, batch_size=train_params_recognition['batch_size'])
 
         self.fit(dataloader, train_params_recognition)
     
@@ -309,79 +327,104 @@ class RecognitionModel(Module):
         x_tilde = self.neural_net(y.transpose(-1, -2)) # (ntrials, batch_size, x_dim)
         return x_tilde.transpose(-1, -2) # (ntrials, x_dim, batch_size)
     
-    def kalman_recursion(self, x_tilde: Tensor, prev_mu=None, prev_Sigma=None):
-        # return Kalman filter mean and covariance
-        n_trials, x_dim, batch_size = x_tilde.shape
-        _xtilde = x_tilde.permute(2, 0, 1) # (batch_size, ntrials, x_dim)
-
-        A = self.gen_model.A # (1, b, b)
-        W = self.gen_model.W # (1, x_dim, b)
-        Q = self.gen_model.Q # (1, b, b)
-        R = self.gen_model.R # (1, x_dim, x_dim)
+    def sample_matheron_pert(self, n_mc: int):
+        z_prior = self.gen_model.sample_z(n_mc).transpose(-1, -2) # (n_mc, ntrials, T, b) # TODO: how to deal with batch_size?
+        pertubation = self.gen_model.W[None, ...] @ z_prior[..., None] # (n_mc, ntrials, T, x_dim, 1)
+        noise = torch.linalg.cholesky(self.gen_model.R) @ torch.randn(*pertubation.shape).to(device) # (n_mc, ntrials, T, x_dim, 1)
+        return (pertubation + noise).squeeze(-1).transpose(-1, -2) # (n_mc, ntrials, x_dim, T)
+    
+    def kalman_covariance(self, T=None): # return all matrices independent of observations
+        if T is None:
+            T = self.gen_model.T
+        A = self.gen_model.A.squeeze(0) # (b, b)
+        W = self.gen_model.W.squeeze(0) # (x_dim, b)
+        Q = self.gen_model.Q.squeeze(0) # (b, b)
+        R = self.gen_model.R.squeeze(0) # (x_dim, x_dim)
 
         # Kalman filter
-        mus = torch.zeros(batch_size, n_trials, self.gen_model.b).to(device)
-        Sigmas = torch.zeros(batch_size, n_trials, self.gen_model.b, self.gen_model.b).to(device)
+        Sigmas_filt = torch.zeros(T, self.gen_model.b, self.gen_model.b).to(device) # Sigmas_filt[t] = Sigma_t^t
+        Sigmas_diffused = torch.zeros(T - 1, self.gen_model.b, self.gen_model.b).to(device) # Sigmas_diffused[t] = Sigma_{t+1}^t
+        Ks = torch.zeros(T, self.gen_model.b, self.gen_model.x_dim).to(device) # Ks[t] = K_t
 
-        start_t = 0 # first index of mus and Sigmas to fill in
-        if prev_mu is None or prev_Sigma is None:
-            # Sub in the first time step
-            mu0 = self.gen_model.mu0 # (1, b)
-            Sigma0 = self.gen_model.Sigma0 # (1, b, b)
-            mus[0] = mu0.expand_as(mus[0]) # (ntrials, b)
-            Sigmas[0] = Sigma0.expand_as(Sigmas[0]) # (ntrials, b, b)
-            start_t = 1
+        # Sub in the first time step
+        Sigma0 = self.gen_model.Sigma0.squeeze(0) # (b, b)
+        K = Sigma0 @ W.T @ torch.linalg.inv(R + W @ Sigma0 @ W.T) # (b, x_dim)
+        Sigmas_filt[0] = Sigma0 - K @ W @ Sigma0 # (b, b)
+        Ks[0] = K
 
-            prev_mu = mus[0] 
-            prev_Sigma = Sigmas[0] 
-
-        const = W.transpose(-1, -2) @ torch.linalg.inv(R) @ W
-        for t in range(start_t, batch_size):
-            # Calculate covariance
-            precision = const + torch.linalg.inv(A @ prev_Sigma @ A.transpose(-1, -2) + Q)
-            Sigma = torch.linalg.inv(precision)
-
-            # Calculate mean
-            kalman_gain = Sigma @ W.transpose(-1,-2) @ torch.linalg.inv(R) 
-            prediction = A @ prev_mu[..., None] # (ntrials, b, 1)
-            prediction_error = _xtilde[t].unsqueeze(-1) - W @ prediction # (ntrials, x_dim, 1)
-            correction = kalman_gain @ prediction_error # (ntrials, b, 1)
-            mu = (prediction + correction).squeeze(-1) # (ntrials, b)
-
-            # Store
-            mus[t] = mu
-            Sigmas[t] = Sigma
-
-            # Update
-            prev_mu = mu
-            prev_Sigma = Sigma
-
-        if self.smoothing:
-            # TODO: implement smoothing
-            pass
-            
+        # Remaining time steps
+        for t in range(1, T):
+            Sigmas_diffused[t-1] = A @ Sigmas_filt[t-1] @ A.T + Q # (b, b)
+            K = Sigmas_diffused[t-1] @ W.T @ torch.linalg.inv(R + W @ Sigmas_diffused[t-1] @ W.T) # (b, x_dim)
+            Sigmas_filt[t] = Sigmas_diffused[t-1] - K @ W @ Sigmas_diffused[t-1] # (b, b)
+            Ks[t] = K
         
-        return mus, Sigmas
+        # Kalman smoother
+        Cs = torch.zeros(T- 1, self.gen_model.b, self.gen_model.b).to(device) # Cs[t] = C_t
+        for t in range(T - 2, -1, -1):
+            Cs[t] = Sigmas_filt[t] @ A.T @ torch.linalg.inv(Sigmas_diffused[t]) # (b, b)
+
+        return Sigmas_filt, Sigmas_diffused, Ks, Cs # (T, b, b), (T-1, b, b), (T, b, x_dim), (T-1, b, b)
+
+
+    def kalman_means(self, x_hat: Tensor, Ks: Tensor, Cs: Tensor):
+        # TODO: support batching
+        n_mc_z, n_trials, x_dim, batch_size = x_hat.shape
+        _xhat = x_hat.permute(-1, 0, 1, 2) # (batch_size, n_mc_z, ntrials, x_dim)
+
+        A = self.gen_model.A.squeeze(0) # (b, b)
+        W = self.gen_model.W.squeeze(0) # (x_dim, b)
+
+        # Kalman filter
+        mus_filt = torch.zeros(batch_size, n_mc_z, n_trials, self.gen_model.b).to(device) # mu_filt[t] = mu_t^t
+        mus_diffused = torch.zeros(batch_size-1, n_mc_z, n_trials, self.gen_model.b).to(device) # mu_diffused[t] = mu_{t+1}^t
+
+        # Sub in the first time step
+        mu0 = self.gen_model.mu0.squeeze(0) # (b)
+        mus_filt[0] = mu0 + (Ks[0] @ (_xhat[0] - W @ mu0)[..., None]).squeeze(-1) # (n_mc_z, ntrials, b)
+
+        start_t = 1
+
+        for t in range(start_t, batch_size):
+            mus_diffused[t-1] = (A @ mus_filt[t-1][..., None]).squeeze(-1) # (n_mc_z, ntrials, b)
+            mus_filt[t] = mus_diffused[t-1] + (Ks[t] @ (_xhat[t][..., None] - W @ mus_diffused[t-1][..., None])).squeeze(-1) # (n_mc_z, ntrials, b)
+
+        # Kalman smoother
+        mus_smooth = torch.zeros(batch_size, n_mc_z, n_trials, self.gen_model.b).to(device) # mu_smooth[t] = mu_t^T
+        mus_smooth[-1] = mus_filt[-1] # (n_mc_z, ntrials, b)
+        for t in range(batch_size - 2, -1, -1):
+            mus_smooth[t] = mus_filt[t] + (Cs[t] @ (mus_smooth[t+1] - mus_diffused[t])[..., None]).squeeze(-1) # (n_mc_z, ntrials, b)
+            
+        return mus_filt, mus_smooth, mus_diffused # (batch_size, n_mc_z, ntrials, b), (batch_size, n_mc_z, ntrials, b), (batch_size-1, n_mc_z, ntrials, b)
     
-    def entropy(self, mus: Tensor, Sigmas: Tensor):
-        # mus is (batch_size, ntrials, b)
-        # Sigmas is (batch_size, ntrials, b, b)
-        # TODO: check if this is correct
-        jitter = torch.eye(mus.shape[-1]).to(device) * 1e-2 # TODO: very high jitter
-        entropy = MultivariateNormal(mus, Sigmas + jitter).entropy() # (batch_size, ntrials)
-        entropy = entropy.sum(dim=0) # sum over time
-        return entropy # (ntrials,)
+    def entropy(self, samples, mus_filt, Sigmas_filt, mus_diffused, Sigmas_diffused):
+        # samples is (batch_size, n_mc_z, ntrials, b)
+        # mus_filt is (batch_size, n_mc_z, ntrials, b)
+        # Sigmas_filt is (batch_size, b, b)
+        # mus_diffused is (batch_size-1, n_mc_z, ntrials, b)
+        # Sigmas_diffused is (batch_size-1, b, b)
 
-    def sample_z(self, n_mc: int, mus: Tensor, Sigmas: Tensor):
-        # TODO
-        # mus is (batch_size, ntrials, b)
-        # Sigmas is (batch_size, ntrials, b, b)
+        # Add jitter to the covariance matrices
+        Sigmas_filt = Sigmas_filt + 1E-6 * torch.eye(Sigmas_filt.shape[-1]).to(device)
+        Sigmas_diffused = Sigmas_diffused + 1E-6 * torch.eye(Sigmas_diffused.shape[-1]).to(device)
+        
+        filter_dist = MultivariateNormal(mus_filt, covariance_matrix=Sigmas_filt[:, None, None, ...])
+        filter_log_prob = filter_dist.log_prob(samples).sum(dim=0) # (n_mc_z, ntrials)
 
-        samples = torch.randn(n_mc, *mus.shape).to(device) # (n_mc, batch_size, ntrials, b)
-        jitter = torch.eye(mus.shape[-1]).to(device) * 1e-2 # TODO: very high jitter
-        Sigma_half = torch.linalg.cholesky(Sigmas + jitter) # (batch_size, ntrials, b, b)
-        samples = mus[None, ...] + (Sigma_half[None, ...] @  samples[..., None]).squeeze(-1) # (n_mc, batch_size, ntrials, b)
-        return samples.permute(0, 2, 3, 1) # (n_mc, ntrials, b, batch_size)
+        diffused_dist = MultivariateNormal(mus_diffused, covariance_matrix=Sigmas_diffused[:, None, None, ...])
+        diffused_log_prob = diffused_dist.log_prob(samples[1:, ...]).sum(dim=0) # (n_mc_z, ntrials)
+
+        # TODO: this code is repeated computation
+        # Natural parameters for p(z_t|z_{t-1})
+        A = self.gen_model.A.squeeze(0) # (b, b)
+        Q = self.gen_model.Q.squeeze(0) # (b, b)
+        mus = (A @ samples[:-1, ..., None]).squeeze(-1) # (batch_size-1, n_mc_z, ntrials, b)
+        prior_dist = MultivariateNormal(mus, covariance_matrix=Q)
+        prior_log_prob = prior_dist.log_prob(samples[1:, ...]).sum(dim=0) # (n_mc_z, ntrials)
+
+        log_prob = filter_log_prob - diffused_log_prob + prior_log_prob # (n_mc_z, ntrials)
+        return -log_prob.mean(dim=0) # (ntrials,) 
+      
 
     def fit(self, data: DataLoader, train_params_recognition):
         lrate = train_params_recognition['lrate']
@@ -393,32 +436,40 @@ class RecognitionModel(Module):
         optimizer = optimizer(self.neural_net.parameters(), lr=lrate)
         scheduler = StepLR(optimizer, step_size=train_params_recognition['step_size'], gamma=train_params_recognition['gamma'])
 
-        LLs = []
+        self.LLs = []
+        Sigmas_filt, Sigmas_diffused, Ks, Cs = self.kalman_covariance() # (T, b, b), (T-1, b, b), (T, b, x_dim), (T-1, b, b)
+
         for i in range(max_steps):
             prev_mu = None
             prev_Sigma = None
             prev_z = None
             for y in data: # loop over batches
+                # NN pseudo observations
+                y = y.transpose(-1, 0) # (ntrials, N, batch_size)
                 x_tilde = self.get_x_tilde(y) # (ntrials, x_dim, batch_size)
-                mus, Sigmas = self.kalman_recursion(x_tilde, prev_mu, prev_Sigma) # (batch_size, ntrials, b), (batch_size, ntrials, b, b)
-                z_samples = self.sample_z(n_mc_z, mus, Sigmas) # (n_mc_z, ntrials, b, batch_size)
-                loss = -self.LL(n_mc_x, z_samples, y, prev_z, mus, Sigmas).mean() # TODO: should I use mean??
+                # Matheron psuedo observations
+                matheron_pert = self.sample_matheron_pert(n_mc_z) # (n_mc_z, ntrials, x_dim, T) # TODO: do I need to do this every time?
+                x_hat = x_tilde[None, ...] - matheron_pert[..., :x_tilde.shape[-1]] # (n_mc_z, ntrials, x_dim, batch_size) TODO: how to deal with batch_size?
+                loss = -self.LL(n_mc_x, x_hat, y, Sigmas_filt, Sigmas_diffused, Ks, Cs).mean() # TODO: should I use mean??
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
-                prev_z = z_samples[..., -1].detach() # (n_mc_z, ntrials, b)
-                prev_mu = mus[-1, ...].detach() # (ntrials, b)
-                prev_Sigma = Sigmas[-1, ...].detach() # (ntrials, b, b)
+                # prev_z = z_samples[..., -1].detach() # (n_mc_z, ntrials, b)
+                # prev_mu = mus[-1, ...].detach() # (ntrials, b)
+                # prev_Sigma = Sigmas[-1, ...].detach() # (ntrials, b, b)
             scheduler.step()
-            LLs.append(-loss.item())
+            self.LLs.append(-loss.item())
             if i % train_params_recognition['print_every'] == 0:
-                print('step', i, 'LL', LLs[-1])
+                print('step', i, 'LL', self.LLs[-1])
 
-    def LL(self, n_mc_x: int, z_samples: Tensor, y: Tensor, prev_z: Tensor, mus: Tensor, Sigmas: Tensor):
+    def LL(self, n_mc_x: int, x_hat:Tensor, y: Tensor, Sigmas_filt: Tensor, Sigmas_diffused: Tensor, Ks: Tensor, Cs: Tensor):
         # y is (ntrials, N, batch_size)
-        # prev_z is (n_mc_z, ntrials, b)
-        entropy = self.entropy(mus, Sigmas) # (ntrials,)
-        joint_LL = self.gen_model.joint_LL(n_mc_x, z_samples, y, prev_z) # (ntrials,)
+        # x_hat is (n_mc_z, ntrials, x_dim, batch_size)
+
+        # mus_smooth are the samples from the posterior through matheron sampling
+        mus_filt, mus_smooth, mus_diffused = self.kalman_means(x_hat, Ks, Cs)
+        entropy = self.entropy(mus_smooth, mus_filt, Sigmas_filt, mus_diffused, Sigmas_diffused) # (ntrials,)
+        joint_LL = self.gen_model.joint_LL(n_mc_x, mus_smooth.permute(1,2,3,0), y, prev_z=None) # (ntrials,) # TODO: batching
         return entropy + joint_LL
     
     def freeze_params(self):
@@ -426,11 +477,19 @@ class RecognitionModel(Module):
             param.requires_grad = False
 
     def test_z(self, test_y: Tensor):
+        # TODO batching
         # return posterior mean on test data
         
         # test_y is (ntrials, N, T_test)
-        x_tilde = self.get_x_tilde(test_y)
-        mus, _ = self.kalman_recursion(x_tilde) # TODO: should I use prev_mu and prev_Sigma?
-        return mus.permute(1, 2, 0) # (ntrials, b, T_test)
-
+        x_tilde = self.get_x_tilde(test_y) # (ntrials, x_dim, T_test)
+        _, _, Ks, Cs = self.kalman_covariance(T=test_y.shape[-1]) # TODO: should I use prev_mu and prev_Sigma?
+        _ , mus_smooth, _ = self.kalman_means(x_tilde[None, ...], Ks, Cs) # (T_test, 1, ntrials, b)
+        mus_smooth = mus_smooth.squeeze(1) # (T_test, ntrials, b)
+        return mus_smooth.permute(1, 2, 0) # (ntrials, b, T_test)
+    
+    def plot_LL(self):
+        plt.plot(self.LLs)
+        plt.xlabel('Step')
+        plt.ylabel('LL')
+        plt.show()
 

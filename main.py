6a263e846ad770a9529e8cf777e70d182f53eb2c
ayush_1301 <ayush.ag1301@gmail.com>
2024-03-11 15:33:47 +0000

    delta R diagonal exp + random changes

--- main.py
+++ main.py
@@ -383,7 +383,9 @@ class LDS(GenerativeModel):
     def R(self):
         # R =  self.var_x * torch.eye(self.x_dim).unsqueeze(0).to(device) # (1, x_dim, x_dim)
         if self.full_R:
-            R = (self.R_half @ self.R_half.T).unsqueeze(0).to(device) # (1, x_dim, x_dim)
+            R_half = torch.tril(self.R_half, diagonal=-1) + torch.diag(torch.exp(torch.diag(self.R_half)))
+            R = (R_half @ R_half.T).unsqueeze(0).to(device) # (1, x_dim, x_dim)
+            # R = (self.R_half @ self.R_half.T).unsqueeze(0).to(device) # (1, x_dim, x_dim)
         else:
             R = torch.diag(self.var_x).unsqueeze(0).to(device) # (1, x_dim, x_dim) 
         jitter = torch.eye(R.shape[-1]).to(R.device) * 1e-6
@@ -662,9 +664,25 @@ class RecognitionModel(Module):
                 return torch.diag_embed(new_var_x) # (batch_size, ntrials, x_dim, x_dim)
 
             else:
-                # delta_R = pseudo_obs['delta_R'] # (batch_size, ntrials, x_dim)
-                pass
-    
+                # Assume implementation of full_R where diag elements are exponentiated
+                delta_R = pseudo_obs['delta_R'] # (batch_size, ntrials, x_dim)
+                if delta_R.shape[-1] == self.gen_model.x_dim:
+                    delta_R = torch.diag_embed(torch.exp(delta_R)) # (batch_size, ntrials, x_dim, x_dim)
+                elif delta_R.shape[-1] == self.gen_model.x_dim * (self.gen_model.x_dim + 1) / 2:
+                    delta_R_reshaped = torch.zeros(delta_R.shape[0], delta_R.shape[1], self.gen_model.x_dim, self.gen_model.x_dim).to(device)
+                    rows, cols = torch.tril_indices(self.gen_model.x_dim, self.gen_model.x_dim)
+                    delta_R_reshaped[..., rows, cols] = delta_R
+                    delta_R = torch.tril(delta_R_reshaped, diagonal=-1) + torch.diag_embed(torch.exp(torch.diagonal(delta_R_reshaped, dim1=-2, dim2=-1))) # (batch_size, ntrials, x_dim, x_dim)
+                else:
+                    raise ValueError('delta_R has wrong shape')
+                    
+                self.dRs.append((torch.max(delta_R).item(), torch.min(delta_R).item()))
+                R_half = torch.tril(self.gen_model.R_half, diagonal=-1) + torch.diag_embed(torch.exp(torch.diag(self.gen_model.R_half))) + delta_R # (batch_size, ntrials, x_dim, x_dim)
+                # new_log_diag = torch.diag(self.gen_model.R_half) + delta_R # (batch_size, ntrials, x_dim)
+                # new_diag = torch.exp(new_log_diag)
+                # R_half = torch.tril(self.gen_model.R_half, diagonal=-1) + torch.diag_embed(new_diag)
+                return R_half @ R_half.transpose(-1,-2) + 1e-6 * torch.eye(self.gen_model.x_dim).to(device) # (batch_size, ntrials, x_dim, x_dim)
+            
     def gen_model_W(self, pseudo_obs):
         if not self.cov_change or pseudo_obs is None or pseudo_obs['delta_W'] is None:
             return self.gen_model.W.squeeze(0)
@@ -856,7 +874,12 @@ class RecognitionModel(Module):
             if i % train_params_recognition['print_every'] == 0:
                 print('step', i, 'LL', self.LLs[-1], 'Entropy', self.entropy_vals[-1], 'Joint LL', self.joint_LL_vals[-1])
                 if train_params_recognition['print_deltas']:
-                    print('dWs', self.dWs[-1], 'dRs', self.dRs[-1])
+                    print_str = ''
+                    if len(self.dWs) > 0:
+                        print_str += 'dWs: ' + str(self.dWs[-1])
+                    if len(self.dRs) > 0:
+                        print_str += 'dRs: ' + str(self.dRs[-1])
+                    print(print_str)
 
     def LL(self, n_mc_x: int, x_hat:Tensor, y: Tensor, Ks: Tensor, Cs: Tensor, Sigmas_tilde_chol: Tensor, pseudo_obs=None):
         # y is (ntrials, N, batch_size)
@@ -903,6 +926,8 @@ class Preprocessor(Module):
         self.z_dim = z_dim # dimension of the latent space
 
         self.A = torch.nn.Parameter(0.8* torch.randn(self.z_dim, self.z_dim).to(device) / np.sqrt(self.z_dim))
+        # self.pre_A_eig = torch.nn.Parameter(torch.randn(self.z_dim).to(device))
+        # self._S = torch.nn.Parameter(torch.randn(self.z_dim, self.z_dim).to(device) / np.sqrt(self.z_dim))
         self.B = torch.nn.Parameter(noise_scale * torch.eye(self.z_dim).to(device))
         self.W = torch.nn.Parameter(torch.randn(self.v_dim, self.z_dim).to(device) / np.sqrt(self.z_dim))
         # self.mu0 = torch.nn.Parameter(torch.rand(self.z_dim).to(device))
@@ -924,23 +949,62 @@ class Preprocessor(Module):
     #     jitter = torch.eye(self.v_dim).to(self.sigma_v.device) * 1e-6
     #     return self.var_v * torch.eye(self.v_dim).to(device) + jitter
     
+    # @property
+    # def A(self):
+    #     l =  torch.diag(torch.tanh(self.pre_A_eig))
+    #     S = torch.tril(self._S)  - torch.tril(self._S).T
+    #     Q = torch.exp(S) # (z_dim, z_dim)
+    #     A =  Q @ l @ Q.inverse()
+    #     print(torch.max(torch.abs(torch.linalg.eigvals(A))))
+    #     return A
+
+
+    def reg(self, x):
+        # Compute the exponential of the diagonal elements and add a small constant
+        xd = torch.exp(torch.diag(x)) + 1e-2
+        # Extract the strictly upper triangular part of x
+        xt = torch.tril(x)
+        # Recombine into a new matrix with adjusted diagonal and original upper triangular parts
+        return torch.diag(xd) + xt
+    
     @property
     def R(self):
-        self.R_half.data = torch.tril(self.R_half.data)
+        # self.R_half.data = torch.tril(self.R_half.data)
+        # jitter = torch.eye(self.v_dim).to(self.R_half.device) * 1e-6
+        # return self.R_half @ self.R_half.T + jitter
+
+        R_half = torch.tril(self.R_half)
         jitter = torch.eye(self.v_dim).to(self.R_half.device) * 1e-6
-        return self.R_half @ self.R_half.T + jitter
+        return R_half @ R_half.T + jitter
+    
+        # R_half = self.reg(self.R_half)
+        # return R_half @ R_half.T
     
     @property
     def Q(self):
-        self.B.data = torch.tril(self.B.data)
+        # self.B.data = torch.tril(self.B.data)
+        # jitter = torch.eye(self.z_dim).to(self.B.device) * 1e-6
+        # return self.B @ self.B.T + jitter
+
+        B = torch.tril(self.B)
         jitter = torch.eye(self.z_dim).to(self.B.device) * 1e-6
-        return self.B @ self.B.T + jitter
+        return B @ B.T + jitter
+
+        # B = self.reg(self.B)
+        # return B @ B.T
     
     @property
     def Sigma0(self):
-        self.Sigma0_half.data = torch.tril(self.Sigma0_half.data)
+        # self.Sigma0_half.data = torch.tril(self.Sigma0_half.data)
+        # jitter = torch.eye(self.z_dim).to(self.Sigma0_half.device) * 1e-6
+        # return self.Sigma0_half @ self.Sigma0_half.T + jitter
+
+        Sigma0_half = torch.tril(self.Sigma0_half)
         jitter = torch.eye(self.z_dim).to(self.Sigma0_half.device) * 1e-6
-        return self.Sigma0_half @ self.Sigma0_half.T + jitter
+        return Sigma0_half @ Sigma0_half.T + jitter
+
+        # Sigma0_half = self.reg(self.Sigma0_half)
+        # return Sigma0_half @ Sigma0_half.T
     
 
     def training_params(self, **kwargs): # code from mgp
@@ -954,7 +1018,9 @@ class Preprocessor(Module):
             'print_every': 1,
             'lrate': 5E-2,
             'accumulate_gradient': True,
-            'batch_mc': None
+            'batch_mc': None,
+            'betas': (0.9, 0.999),
+            'eps': 1e-8,
         }
 
         for key, value in kwargs.items():
@@ -990,7 +1056,8 @@ class Preprocessor(Module):
         max_steps = train_params['max_steps']
 
         optimizer = train_params['optimizer']
-        optimizer = optimizer(self.parameters(), lr=lrate)
+        # optimizer = optimizer(self.parameters(), lr=lrate)
+        optimizer = optimizer(self.parameters(), lr=lrate, betas=train_params['betas'], eps=train_params['eps'])
         scheduler = StepLR(optimizer, step_size=train_params['step_size'], gamma=train_params['gamma'])
 
         self.LLs = []
@@ -1011,8 +1078,10 @@ class Preprocessor(Module):
     def LL(self, v: Tensor, mus_diffused, Sigmas_diffused):
         # v is (ntrials, v_dim, T)
         # return log likelihood of v
-        # mus_diffused is (T, ntrials, z_dim)
-        # Sigmas_diffused is (T, z_dim, z_dim)
+        # mus_diffused is (T-1, ntrials, z_dim)
+        # Sigmas_diffused is (T-1, 1, z_dim, z_dim)
+        # print(mus_diffused.shape, Sigmas_diffused.shape)
+        Sigmas_diffused = Sigmas_diffused.squeeze(1) # (T-1, z_dim, z_dim)
         ntrials = v.shape[0]
         T = v.shape[-1]
 
@@ -1025,8 +1094,10 @@ class Preprocessor(Module):
         # ln p(v_t|v_{1: t-1})
         ts = torch.arange(1, T).to(device)
         ms = (self.W @ mus_diffused[ts - 1][..., None]).squeeze(-1) # (T-1, ntrials, v_dim)
+        # print(ms.shape)
         jitter = torch.eye(self.v_dim).to(self.R.device) * 1e-6
-        covs = self.W @ Sigmas_diffused[ts - 1] @ self.W.T + self.R + jitter # (T-1, v_dim, v_dim)
+        covs = (self.W @ Sigmas_diffused[ts - 1] @ self.W.T) + self.R #+ jitter # (T-1, v_dim, v_dim)
+        # print(covs.shape)
         covs_chol = torch.linalg.cholesky(covs)
         dists = MultivariateNormal(ms, scale_tril=covs_chol[:, None, ...])
         second = dists.log_prob(v.permute(2, 0, 1)[1:, ...]) # (T-1, ntrials)
